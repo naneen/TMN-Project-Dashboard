00a4d8e142e308519b70ee659e56da228f13d014
iwilllookingup <baspente_02@hotmail.com>
2015-06-15 18:28:49 +0700

    asdasdasda

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
+		
 		model.addAttribute("message", "EIEIEIS!");
 		return "hello";
 	}
