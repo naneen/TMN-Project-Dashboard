e8d2740aaed56fc04ea82cf18ec170e9c8c5ac51
baspente_02@hotmail.com <baspente_02@hotmail.com>
2015-06-25 14:07:38 +0700

    create unit test for HelloController Class: placeCut()

---
+++ src/test/java/com/springapp/mvc/controller/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.springapp.mvc.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Bas on 25/6/2558.
+ */
+public class HelloControllerTest {
+
+    @Test
+    public void testPlaceCut() {
+        HelloController HelloController = new HelloController();
+        assertEquals("Central 1",HelloController.PlaceCut("True Shop Station in True Coffee Central 1"));
+        assertEquals("Central 2",HelloController.PlaceCut("True Shop Mini Central 2"));
+        assertEquals("Central 3", HelloController.PlaceCut("True Shop Central 3"));
+        assertEquals("Central 4", HelloController.PlaceCut("TrueCoffee @ Central 4"));
+        assertEquals("Central 5", HelloController.PlaceCut("TrueCoffee Central 5"));
+        assertEquals("Central 6",HelloController.PlaceCut("True Coffee Central 6"));
+        assertEquals("Central 7",HelloController.PlaceCut("True Move Shop Central 7"));
+        assertEquals("Central 8",HelloController.PlaceCut("True Life Central 8"));
+    }
+}
--- src/test/java/com/springapp/mvc/controller/ValueComparatorTest.java
+++ src/test/java/com/springapp/mvc/controller/ValueComparatorTest.java
@@ -19,14 +19,16 @@ public class ValueComparatorTest {
     @Test
     public void testCompare() throws Exception {
         HashMap<String,Integer> map = new HashMap<String, Integer>();
-        map.put("55",55);
-        map.put("555",555);
-        map.put("5",5);
+        map.put("55", 55);
+        map.put("555", 555);
+        map.put("5", 5);
         ValueComparator valueCom = new ValueComparator(map);
 
-        assertEquals("'5' is lower than '55', should return 1",1,valueCom.compare("5","55"));
-        assertEquals("'555' is greater than '55', should return -1",-1,valueCom.compare("555","55"));
-        assertEquals("'555' is equal to '555', should return -1",-1,valueCom.compare("555","555"));
+        assertEquals("'5' is lower than '55', should return 1",1,valueCom.compare("5", "55"));
+        assertEquals("'555' is greater than '55', should return -1",-1,valueCom.compare("555", "55"));
+        assertEquals("'555' is equal to '555', should return -1",-1,valueCom.compare("555", "555"));
 
     }
+
+
 }
