a5ca689d2ba279c43243580a7cb0805b9a4dc9c6
loginjames <me.jamesmy@hotmail.com>
2015-07-14 10:56:57 +0700

    3

---
+++ update-caller.py
@@ -0,0 +1,13 @@
+import os
+import ConfigParser
+import subprocess
+
+configurationFile='/tmp/prepaid-card-service/install.ini'
+
+config = ConfigParser.ConfigParser()
+print('Reading configuration from ' + configurationFile)
+config.read(configurationFile)
+
+profileName = config.get('WebSphere', 'profileName')
+
+subprocess.call(["/data/IBM/WebSphere/AppServer/profiles/" + profileName + "/bin/wsadmin.sh", "-user", "wasadmin", "-password", "adminwas", "-lang", "jython", "-f", "/tmp/prepaid-card-service/update.py"] )
